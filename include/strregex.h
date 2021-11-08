7e34efbc58fccff9abed332c6e76b3166ae370c7
Clifford Heath <clifford.heath@gmail.com>
2021-11-08 18:02:27 +1100

    Added regular expression spike, with feature toggles and RE lexer

---
+++ include/strregex.h
@@ -0,0 +1,135 @@
+#if !defined(STRREGEX_H)
+#define STRREGEX_H
+/*
+ * Unicode String regular expressions
+ */
+#include	<strpp.h>
+
+/*
+ * RxCompiled compiles a regular expressions for the machine to execute.
+ */
+class RxCompiled
+{
+protected:
+	enum class RxOp: unsigned char;
+
+public:
+	enum RxFeature {
+		NoFeature	= 0x0000000,
+		// Kinds of characters or classes:
+		// AnyChar	= 0x0000000,	// . means any char, default
+		CEscapes	= 0x0000001,	// \0 \b \e \f \n \t \r
+		Shorthand	= 0x0000002,	// \s, later \d \w
+		OctalChar	= 0x0000004,	// \177 (1-3 digits)
+		HexChar		= 0x0000008,	// \xNN	(1-2 digits)
+		UnicodeChar	= 0x0000010,	// \uNNNNN (1-5 digits)
+		PropertyChars	= 0x0000020,	// \p{Property_Name}, see https://unicode.org/reports/tr18/
+//		CharClass	= 0x0000040,	// [...], [^...]
+//		PosixClasses	= 0x0000080,	// [:digit:], [=e=], etc, in classes
+		// Kinds of multiplicity:
+		ZeroOrMore	= 0x0000100,	// *, zero or more
+		OneOrMore	= 0x0000200,	// +, one or more
+		ZeroOrOneQuest	= 0x0000400,	// ?, zero or one
+		CountRepetition	= 0x0000800,	// {n, m}
+		// Groups of regular expressions:
+		Alternates	= 0x0001000,	// re1|re2
+		Group		= 0x0002000,	// (re)
+		Capture		= 0x0004000,	// (?<group_name>re)
+		NonCapture	= 0x0008000,	// (?:re)
+		NegLookahead	= 0x0010000,	// (?!re)
+		Subroutine	= 0x0020000,	// (?&group_name)
+		// Line start/end
+		BOL		= 0x0040000,	// ^ beginning of line
+		EOL		= 0x0080000,	// $ end of line
+		AllFeatures	= 0x00FFFFF,
+		// Options relevant to regexp interpretation
+		AnyIsQuest	= 0x01000000,	// ? means any char (so cannot be ZeroOrOneQuest)
+		ZeroOrMoreAny	= 0x02000000,	// * means zero or more any, aka .*
+		AnyIncludesNL	= 0x04000000,	// Any character includes newline
+		CaseInsensitive	= 0x08000000,	// Perform case-insensitive match
+//		Digraphs	= 0x10000000,	// e.g. ll in Spanish, ss in German, ch in Czech
+		ExtendedRE	= 0x20000000,	// Whitespace allowed, but may be matched by \s
+	};
+	~RxCompiled();
+	RxCompiled(StrVal re, RxFeature reject_features = NoFeature, RxFeature ignore_features = NoFeature);
+
+	// Lexical scanner for a regular expression. Returns false if error_message gets set.
+	bool		scan_rx(bool (*func)(RxOp op, StrVal param));
+
+	const char*	ErrorMessage() const { return error_message; }
+
+protected:
+	StrVal		re;
+	RxFeature	features_enabled;	// Features that are not enabled are normally ignored
+	RxFeature	features_rejected;	// but these features cause an error if used
+	const char*	error_message;
+
+	// Regular expression opcodes
+	enum class RxOp: unsigned char {
+		RxoStart,		// Place to start the DFA
+		RxoChar,		// A specific char
+		RxoCharProperty,	// A char with a named Unicode property
+		RxoBOL,			// Beginning of Line
+		RxoEOL,			// End of Line
+		RxoCharClass,		// Character class
+		RxoNegCharClass,	// Negated Character class
+		RxoAny,			// Any single char
+		RxoZeroOrOne,		// ?
+		RxoZeroOrMore,		// *
+		RxoOneOrMore,		// +
+		RxoCountedRepetition,	// {n, m}
+		RxoNonCapturingGroup,	// (...)
+		RxoNamedCapture,	// (?<name>...)
+		RxoNegLookahead,	// (?!...)
+		RxoAlternate,		// |
+		RxoSubroutine,		// Subroutine call to a named group
+		RxoEndGroup,		// End of a group
+		RxoEnd			// Termination condition
+	};
+
+	struct CharClass {
+		// REVISIT: Implement character classes
+	};
+	class RxInstruction {
+	public:
+		RxOp		op;
+		union {
+			StrVal*		str;
+			struct {
+				uint16_t	min;
+				uint16_t	max;
+			} repetition;
+			CharClass*	cclass;
+		};
+
+		RxInstruction(RxOp);
+		RxInstruction(RxOp, StrVal);
+		RxInstruction(RxOp, int min, int max);
+		RxInstruction(RxOp, CharClass*);
+		~RxInstruction() {
+			// Clean up according to type.
+			if (op == RxOp::RxoNamedCapture || op == RxOp::RxoSubroutine)
+				delete str;
+			else if (op == RxOp::RxoCharClass || op == RxOp::RxoNegCharClass)
+				delete[] cclass;
+		}
+	};
+
+	bool		supported(RxFeature);	// Set error message and return false on rejected feature use
+	bool		enabled(RxFeature);	// Return true if the specified feature is enabled
+};
+
+/*
+ * RxEngine is the machine that evaluates a Regular Expression
+ * against a string, and contains its result.
+ */
+class RxEngine
+{
+public:
+	~RxEngine();
+	RxEngine(const RxCompiled* machine, StrVal target);
+
+private:
+};
+
+#endif	// STRREGEX_H
---
+++ src/strregex.cpp
@@ -0,0 +1,270 @@
+/*
+ * Unicode String regular expressions
+ */
+#include	<strregex.h>
+
+RxCompiled::RxCompiled(StrVal _re, RxFeature reject_features, RxFeature ignore_features)
+: re(_re)
+, features_enabled((RxFeature)(RxFeature::AllFeatures & ~(reject_features | ignore_features)))
+, features_rejected(reject_features)
+, error_message(0)
+{
+	// estimate_limits();
+}
+
+RxCompiled::~RxCompiled()
+{
+}
+
+bool RxCompiled::supported(RxFeature feat)
+{
+	if (features_rejected & feat)
+	{
+		error_message = "Rejected feature";
+		return false;
+	}
+	return enabled(feat);
+}
+
+bool RxCompiled::enabled(RxFeature feat)
+{
+	return (features_enabled & feat) != 0;
+}
+
+bool RxCompiled::scan_rx(bool (*func)(RxOp op, StrVal param))
+{
+	int		i = 0;		// Regex character offset
+	UCS4		ch;		// A single character to match
+	StrVal		param;		// A string argument
+	int		close;		// String offset of the character ending a param
+	int		int_error = 0;
+	CharNum		scanned;
+	bool		ok = true;
+
+	ok = func(RxOp::RxoStart, StrVal::null);
+	for (; i < re.length(); i++)
+	{
+		switch (ch = re[i])	// Breaking from this switch indicates an error and stops the scan
+		{
+		case '^':
+			if (!supported(BOL))
+				goto simple_char;
+			ok = func(RxOp::RxoBOL, StrVal::null);
+			continue;
+
+		case '$':
+			if (!supported(EOL))
+				goto simple_char;
+			ok = func(RxOp::RxoEOL, StrVal::null);
+			continue;
+
+		case '.':
+			if (enabled(AnyIsQuest))
+				goto simple_char;	// if using ?, . is a simple char
+			ok = func(RxOp::RxoAny, StrVal::null);
+			continue;
+
+		case '?':
+			if (enabled(AnyIsQuest))
+				ok = func(RxOp::RxoAny, StrVal::null);	// Shell-style any-char wildcard
+			else
+				ok = func(RxOp::RxoZeroOrOne, StrVal::null);	// Previous elem is optional
+			continue;
+
+		case '*':
+			if (!supported(ZeroOrMore))
+				goto simple_char;
+			if (enabled(ZeroOrMoreAny))
+				ok = func(RxOp::RxoAny, StrVal::null);
+			ok = func(RxOp::RxoZeroOrMore, StrVal::null);
+			continue;
+
+		case '+':
+			if (!supported(OneOrMore))
+				goto simple_char;
+			ok = func(RxOp::RxoOneOrMore, StrVal::null);
+			continue;
+
+		case '{':
+			if (!supported(CountRepetition))
+				goto simple_char;
+			// REVISIT: Implement counted repetition
+			// ok = func(RxOp::RxoOneOrMore, StrVal::null);
+			break;
+
+		case '\\':		// Escape sequence
+			switch (re[++i])
+			{
+			case 'b': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\b'); continue;
+			case 'e': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\e'); continue;
+			case 'f': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\f'); continue;
+			case 'n': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\n'); continue;
+			case 't': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\t'); continue;
+			case 'r': if (!enabled(CEscapes)) goto simple_escape; ok = func(RxOp::RxoChar, '\r'); continue;
+
+			case '0': case '1': case '2': case '3':	// Octal char constant
+			case '4': case '5': case '6': case '7':
+				if (!enabled(OctalChar))
+					goto simple_escape;
+				param = re.substr(i, 3);
+				ch = param.asInt32(&int_error, 8, &scanned);
+				if (int_error || scanned == 0)
+				{		// I don't think this can happen
+					error_message = "Illegal octal character";
+					break;
+				}
+				i += scanned;
+				ok = func(RxOp::RxoChar, ch);
+				continue;
+
+			case 'x':			// Hex byte (1 or 2 hex digits follow)
+				if (!enabled(HexChar))
+					goto simple_escape;
+				param = re.substr(++i, 2);
+				ch = param.asInt32(&int_error, 16, &scanned);
+				if (int_error || scanned == 0)
+				{
+					error_message = "Illegal hexadecimal character";
+					break;
+				}
+				i += scanned;
+				ok = func(RxOp::RxoChar, ch);
+				continue;
+
+			case 'u':			// Unicode (1-5 hex digits follow)
+				if (!enabled(UnicodeChar))
+					goto simple_escape;
+				param = re.substr(++i, 5);
+				ch = param.asInt32(&int_error, 16, &scanned);
+				if (int_error || scanned == 0)
+				{
+					error_message = "Illegal escaped unicode character";
+					break;
+				}
+				i += scanned;
+				ok = func(RxOp::RxoChar, ch);
+				continue;
+
+			case 's':			// Whitespace
+				if (!enabled(Shorthand))
+					goto simple_escape;
+				ok = func(RxOp::RxoCharProperty, ' ');
+				continue;
+
+			case 'p':			// Posix character type
+				if (!enabled(PropertyChars))
+					goto simple_escape;
+				if (re[++i] != '{')
+				{
+			bad_posix:	error_message = "Illegal Posix character specification";
+					break;
+				}
+				param = re.substr(i+1);
+				close = param.find('}');
+				if (close <= 0)
+					goto bad_posix;
+				param = param.substr(0, close); // Truncate name to before '}'
+				i += close;	// Now pointing at the '}'
+				ok = func(RxOp::RxoCharProperty, param);
+				continue;
+
+			default:
+			simple_escape:
+				ok = func(RxOp::RxoChar, re[i]);
+				continue;
+			}
+			break;
+
+		case '[':		// Character class
+			// REVISIT: implement character classes
+			break;
+
+		case '|':
+			if (!supported(Alternates))
+				goto simple_char;
+			ok = func(RxOp::RxoAlternate, StrVal::null);
+			continue;
+
+		case '(':
+			if (!supported(Group))
+				goto simple_char;
+			if (!enabled((RxFeature)(Capture|NonCapture|NegLookahead|Subroutine)) // not doing fancy groups
+			 || re[i+1] != '?')					// or this one is plain
+			{		// Anonymous group
+				ok = func(RxOp::RxoNonCapturingGroup, StrVal::null);
+				continue;
+			}
+
+			i += 2;	// Skip the '(?'
+			if (supported(Capture) && re[i] == '<')		// Capture
+			{
+				param = re.substr(++i);
+				close = param.find('>');
+				if (close <= 0)
+				{
+			bad_capture:	error_message = "Invalid group name";
+					break;
+				}
+				param = param.substr(0, close);		// Truncate name to before >
+				i += close+1;
+				ok = func(RxOp::RxoNamedCapture, param);
+				continue;
+			}
+			else if (supported(NonCapture) && re[i] == ':')	// Noncapturing group
+			{
+				ok = func(RxOp::RxoNonCapturingGroup, StrVal::null);
+				continue;
+			}
+			else if (supported(NegLookahead) && re[i] == '!') // Negative Lookahead
+			{
+				ok = func(RxOp::RxoNegLookahead, StrVal::null);
+				continue;
+			}
+			else if (re[i] == '&')				// Subroutine
+			{
+				param = re.substr(++i);
+				close = param.find(')');
+				if (close <= 0)
+					goto bad_capture;
+				param = param.substr(0, close); // Truncate name to before >
+				ok = func(RxOp::RxoSubroutine, param);
+				i += close;	// Now pointing at the ')'
+				continue;
+			}
+			else
+			{
+				error_message = "illegal group type";
+				break;
+			}
+			continue;
+
+		case ')':
+			if (!enabled(Group))
+				goto simple_char;
+			ok = func(RxOp::RxoEndGroup, StrVal::null);
+			continue;
+
+		default:
+		simple_char:
+			ok = func(RxOp::RxoChar, ch);
+			continue;
+		}
+
+		break;
+	}
+	if (i >= re.length())	// If we didn't complete, there was an error
+	{
+		ok = func(RxOp::RxoEnd, StrVal::null);
+		return true;
+	}
+	return false;
+}
+
+/*
+ * Evaluate the RE, counting the number of instructions, groups, captures, and parallel states
+ * that are required to compile and execute it
+void
+RxCompiled::estimate_limits()
+{
+}
+ */
