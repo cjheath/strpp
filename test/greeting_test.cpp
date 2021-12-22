28788daafe1a7d1697a467aedb2ea0ee49cf76a7
Clifford Heath <clifford.heath@gmail.com>
2021-12-22 20:16:04 +1100

    Renamed test programs

--- test/greeting_test.cpp
+++ test/greeting_test.cpp
@@ -1,3 +1,6 @@
+/*
+ * Simple greeting test with emoji manipulation and case conversion
+ */
 #include        <stdio.h>
 #include        "strpp.h"
 
@@ -5,6 +8,9 @@ void greet(StrVal greeting)
 {       
 	StrVal  decorated = greeting + "! 🎉🍾\n";
 	fputs(decorated.asUTF8(), stdout);
+
+	greeting.toUpper();
+	fputs(greeting.asUTF8(), stdout);
 }
 
 int main()
---
+++ test/reassembly_test.cpp
@@ -0,0 +1,25 @@
+/*
+ * Test the re-assembly of contiguous slices
+ */
+#include        <stdio.h>
+
+#define		protected	public
+#define		private		public
+
+#include        "strpp.h"
+
+int main()
+{
+	StrVal  hello_world("Hello, world");
+	StrVal	hello = hello_world.substr(0, 5);
+	StrVal	comma = hello_world.substr(5, 2);
+	StrVal	world = hello_world.substr(7, 5);
+	StrVal	reassembled = hello + comma;
+	reassembled += world;
+
+	fprintf(stdout, "hello_world\t%p[%d, %d]\n", (StrBody*)hello_world.body, hello_world.offset, hello_world.num_chars);
+	fprintf(stdout, "hello\t\t%p[%d, %d]\n", (StrBody*)hello.body, hello.offset, hello.num_chars);
+	fprintf(stdout, "comma\t\t%p[%d, %d]\n", (StrBody*)comma.body, comma.offset, comma.num_chars);
+	fprintf(stdout, "world\t\t%p[%d, %d]\n", (StrBody*)world.body, world.offset, world.num_chars);
+	fprintf(stdout, "reassembled\t%p[%d, %d]\n", (StrBody*)reassembled.body, reassembled.offset, reassembled.num_chars);
+}
