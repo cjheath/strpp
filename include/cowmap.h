22351757793946fb4f7dabe739d00c41d6b58978
Clifford Heath <clifford.heath@gmail.com>
2025-07-13 15:54:28 +1000

    Implemented assorted functional methods: keys, values, each, all, any, one, select, map, inject

--- include/cowmap.h
+++ include/cowmap.h
@@ -79,6 +79,80 @@ public:
 				return k;
 			}
 
+	// Functional methods (these don't mutate or Unshare the subject):
+	Array<Key>	keys() const
+			{
+				Array<K>	all_keys((Value*)0, 0, size());
+				for (Iter it = begin(); it != end(); it++)
+					all_keys.append(it.first);
+				return all_keys;
+			}
+	Array<Value>	values() const
+			{
+				Array<Value>	all_values((Value*)0, 0, size());
+				for (Iter it = begin(); it != end(); it++)
+					all_values.append(it.second);
+				return all_values;
+			}
+
+	void		each(std::function<void(const Key& k, const Value& v)> operation) const
+			{
+				for (Iter it = begin(); it != end(); it++)
+					operation(it.first, it.second);
+			}
+	bool		all(std::function<bool(const Key& k, const Value& v)> condition) const	// Do all elements satisfy the condition?
+			{
+				for (Iter it = begin(); it != end(); it++)
+					if (!condition(it.first, it.second))
+						return false;
+				return true;
+			}
+	bool		any(std::function<bool(const Key& k, const Value& v)> condition) const	// Does any element satisfy the condition?
+			{
+				for (Iter it = begin(); it != end(); it++)
+					if (condition(it.first, it.second))
+						return true;
+				return false;
+			}
+	bool		one(std::function<bool(const Key& k, const Value& v)> condition) const	// Exactly one element satisfies the condition
+			{
+				bool		found = false;
+				for (Iter it = begin(); it != end(); it++)
+					if (condition(it.first, it.second))
+					{
+						if (found)
+							return false;
+						found = true;
+					}
+				return found;
+			}
+
+	CowMap		select(std::function<bool(const Key& k, const Value& v)> condition) const
+			{
+				CowMap	selected;
+
+				for (Iter it = begin(); it != end(); it++)
+					if (condition(it.first, it.second))
+						selected.put(it.first, it.second);
+				return selected;
+			}
+	template<typename V2 = V, typename K2 = K>
+	CowMap<V2, K2>	map(std::function<std::pair<K2, V2>(const Key& k, const Value& v)> map1) const
+			{
+				CowMap<V2, K2>	output;
+				for (Iter it = begin(); it != end(); it++)
+					output.append(map1(it.first, it.second));
+				return output;
+			}
+	template<typename J>
+	J		inject(const J& start, std::function<J(J&, const Key& k, const Value& v)> injection) const
+			{
+				J	accumulator = start;
+				for (Iter it = begin(); it != end(); it++)
+					accumulator = injection(accumulator, it.first, it.second);
+				return accumulator;
+			}
+
 private:
 	Ref<Body>	body;		// The storage structure for the elements
 
@@ -86,13 +160,6 @@ private:
 			{
 				if (body && body->GetRefCount() <= 1)
 					return;
-
-				/*
-				printf(
-					"Unsharing a CowMap with %d references and %ld entries\n",
-					body->GetRefCount(), body->size()
-				);
-				*/
 
 				// Copy the old body's data
 				Body* newbody = new Body();
