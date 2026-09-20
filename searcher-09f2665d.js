fc852b890249d28514b7940dc82e9bcd95caa8b3
Clifford Heath <clifford.heath@gmail.com>
2026-09-20 21:35:06 +1000

    Implemented StrVal::format and removed stdio completely

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8f916473.js',
+                path_to_root + 'searchindex-cac6e935.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
