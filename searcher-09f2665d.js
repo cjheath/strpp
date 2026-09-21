fde4e934b4f30889b177908243bb1d2943f7f349
Clifford Heath <clifford.heath@gmail.com>
2026-09-21 15:12:40 +1000

    Added format and unsigned handling

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-45b4b769.js',
+                path_to_root + 'searchindex-99bebd1a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
